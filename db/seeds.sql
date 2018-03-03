8e9aacf76741100ba7757abb83062ea51422934e
Elshibani Omar <amr.omar80@gmail.com>
2018-03-02 22:24:55 -0700

    Add app files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Park Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mini Park Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Duble Duble', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Park Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Park Burger', true);
